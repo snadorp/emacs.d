0e22b66f5555ccd5267a5e96d04282f875a628aa
Sebastian <sebastian.nadorp@gmail.com>
2013-09-18 18:59:09 +0200

    Fixed bug in Linux skipping the osx check.

--- my-osx.el
+++ my-osx.el
@@ -1,18 +1,18 @@
 ;; only run this stuff when using osx
-(if (eq system-type 'darwin)
-
-    (defun set-exec-path-from-shell-PATH ()
-      "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+(if
+    (eq system-type 'darwin)
+    (
+     (defun set-exec-path-from-shell-PATH
+       "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
        This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-      (interactive)
-      (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-        (setenv "PATH" path-from-shell)
-        (setq exec-path (split-string path-from-shell path-separator))))
+       (interactive)
+       (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+         (setenv "PATH" path-from-shell)
+         (setq exec-path (split-string path-from-shell path-separator))))
 
-  (set-exec-path-from-shell-PATH)
+     (set-exec-path-from-shell-PATH)
 
-  ;; This variable describes the behavior of the command key.
-  (setq mac-option-key-is-meta t)
-  (setq mac-right-option-modifier nil)
-  (setq mac-allow-anti-aliasing t)
-  )
+     ;; This variable describes the behavior of the command key.
+     (setq mac-option-key-is-meta t)
+     (setq mac-right-option-modifier nil)
+     (setq mac-allow-anti-aliasing t)))
