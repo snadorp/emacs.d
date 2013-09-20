2db286eeaaa9eb7c5fbd5ab83edcc46378f8f8c4
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2013-09-20 11:21:01 +0200

    Fixed path in OSX.

--- my-osx.el
+++ my-osx.el
@@ -1,14 +1,16 @@
 ;; only run this stuff when using osx
 (if (eq system-type 'darwin)
+
+    ;; set the PATH to shell execution path
+    ;; http://www.emacswiki.org/emacs/EmacsApp
     (defun set-exec-path-from-shell-PATH
-      "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-       This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
-      (interactive)
-      (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
-        (setenv "PATH" path-from-shell)
-        (setq exec-path (split-string path-from-shell path-separator))))
+      (if (not (getenv "TERM_PROGRAM"))
+          (let ((path (shell-command-to-string
+                       "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
+            (setenv "PATH" path))))
 
   (set-exec-path-from-shell-PATH)
+
 
   ;; This variable describes the behavior of the command key.
   (setq mac-option-key-is-meta t)
