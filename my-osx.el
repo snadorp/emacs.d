59db0cf4c08dbb736fcd263bb20203518f77360f
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2013-09-23 12:13:50 +0200

    Fixed osx path setup.

--- my-osx.el
+++ my-osx.el
@@ -1,16 +1,19 @@
 ;; only run this stuff when using osx
-(if (eq system-type 'darwin)
+(when (eq system-type 'darwin)
 
     ;; set the PATH to shell execution path
     ;; http://www.emacswiki.org/emacs/EmacsApp
-    (defun set-exec-path-from-shell-PATH
+    (defun set-exec-path-from-shell-PATH ()
+      (interactive)
       (if (not (getenv "TERM_PROGRAM"))
-          (let ((path (shell-command-to-string
-                       "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
-            (setenv "PATH" path))))
+          (let ((path
+		 (shell-command-to-string "source $HOME/.bashrc && printf $PATH")
+;;		 (shell-command-to- "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")
+		 ))
+            (setenv "PATH" path)))
+      (message "Path set."))
 
   (set-exec-path-from-shell-PATH)
-
 
   ;; This variable describes the behavior of the command key.
   (setq mac-option-key-is-meta t)
