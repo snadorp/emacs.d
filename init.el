efe478f2143ddc7068d0b70c9e7ad79a5149fe7b
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2013-09-24 10:29:27 +0200

    Removed zone-mode.

--- init.el
+++ init.el
@@ -80,9 +80,6 @@
 (load "my-hooks")
 (load "my-keybindings")
 
-(require 'zone)
-(zone-when-idle 120)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
