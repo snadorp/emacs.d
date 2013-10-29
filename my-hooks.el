0729c2f486d922beaaab125f772e965d769c87de
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2013-10-29 16:12:39 +0100

    Added highlight-indentation hooks.

--- my-hooks.el
+++ my-hooks.el
@@ -1,3 +1,8 @@
 ;; hook to load the required ruby version
 (add-hook 'ruby-mode-hook
           (lambda () (rvm-activate-corresponding-ruby)))
+
+;; highlight-indentation-mode hooks
+(add-hook 'ruby-mode-hook 'highlight-indentation-mode)
+(add-hook 'js-mode-hook 'highlight-indentation-mode)
+(add-hook 'js2-mode-hook 'highlight-indentation-mode)
