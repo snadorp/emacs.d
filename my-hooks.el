def0a4c6d792489f84a4534cc180a5775f00cb79
Sebastian Nadorp <sebastian.nadorp@nugg.ad>
2014-01-20 17:24:36 +0100

    Add flycheck hook.

--- my-hooks.el
+++ my-hooks.el
@@ -2,6 +2,9 @@
 (add-hook 'ruby-mode-hook
           (lambda () (rvm-activate-corresponding-ruby)))
 
+;; globally enable flycheck.
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
 ;; highlight-indentation-mode hooks
 (add-hook 'ruby-mode-hook 'highlight-indentation-mode)
 (add-hook 'js-mode-hook 'highlight-indentation-mode)
